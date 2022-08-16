610bb8ab727898be7ff4860117f36d81fa09db93
Giorgi Tsintsadze <giotsintsadze94@gmail.com>
2022-08-16 15:47:40 +0400

    prepared for new branch

--- database.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `Users` (
-                         `id` INT AUTO_INCREMENT,
-                         `first_name` varchar(255) NOT NULL,
-                         `last_name` varchar(255) NOT NULL,
-                         `email` varchar(255) NOT NULL,
-                         PRIMARY KEY (`id`)
-);
